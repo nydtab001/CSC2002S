19fab8c4165346e62f0dd8300fe3315fbe8cd505
nydtab001 <nydtab001@myuct.ac.za>
2021-08-21 22:57:17 +0200

    added exception catch

--- Main.java
+++ Main.java
@@ -13,34 +13,38 @@ public class Main {
      */
 
     public static void main(String[] args) throws Exception {
-        File file = new File("C:\\Users\\Taboka\\Desktop\\Course\\CSC2002S\\Ass1\\src\\"+args[0]);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String st;
-        int i = 0;
-        int size = Integer.parseInt(br.readLine());
-        double[] x = new double[size];
-        double[] y;
-        DecimalFormat df = new DecimalFormat("#.#####");
-        while ((st = br.readLine()) != null) {
-            st = st.split(" ")[1];
-            st = st.replace(',', '.');
-            x[i] = Double.parseDouble(df.format(Double.parseDouble(st)));
-            i = i + 1;
+        try {
+            File file = new File("C:\\Users\\Taboka\\Desktop\\Course\\CSC2002S\\Ass1\\src\\" + args[0]);
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String st;
+            int i = 0;
+            int size = Integer.parseInt(br.readLine());
+            double[] x = new double[size];
+            double[] y;
+            DecimalFormat df = new DecimalFormat("#.#####");
+            while ((st = br.readLine()) != null) {
+                st = st.split(" ")[1];
+                st = st.replace(',', '.');
+                x[i] = Double.parseDouble(df.format(Double.parseDouble(st)));
+                i = i + 1;
+            }
+            long start = System.currentTimeMillis(); //start of the timing of the thread
+            y = ForkJoinPool.commonPool().invoke(new parallel(x, 0, x.length, Integer.parseInt(args[1])));
+            long finish = System.currentTimeMillis(); //end of the timing of the thread
+            System.out.println("parallel program");
+            System.out.println("filter size: " + args[1]);
+            System.out.println("time taken(ms): " + (finish - start)); //prints the time difference
+            PrintWriter fw = new PrintWriter(args[2]);  // initializes the file to be written to from the application input
+            fw.println(y.length); // writes the length of the array or the subsequent lines of the file
+            for (int j = 0; j < y.length; j++) {
+                String temp = String.valueOf(y[j]);
+                temp = temp.replace('.', ',');
+                temp = j + " " + temp;
+                fw.println(temp); // writes each array element on a new line
+            }
+            fw.close(); // closes the file writer
+        } catch (Exception e) {
+            System.out.println("File"+args[0]+"not found");
         }
-        long start = System.currentTimeMillis(); //start of the timing of the thread
-        y = ForkJoinPool.commonPool().invoke(new parallel(x, 0, x.length,Integer.parseInt(args[1])));
-        long finish = System.currentTimeMillis(); //end of the timing of the thread
-        System.out.println("parallel program");
-        System.out.println("filter size: "+args[1]);
-        System.out.println("time taken(ms): "+(finish-start)); //prints the time difference
-        PrintWriter fw = new PrintWriter(args[2]);  // initializes the file to be written to from the application input
-        fw.println(y.length); // writes the length of the array or the subsequent lines of the file
-        for(int j=0;j<y.length;j++){
-            String temp = String.valueOf(y[j]);
-            temp = temp.replace('.',',');
-            temp = j+" "+temp;
-            fw.println(temp); // writes each array element on a new line
-        }
-        fw.close(); // closes the file writer
     }
 }
